f84926ba1c081831ce442faddcf3f0c5e87acfa4
ddarpan <darpandaswani04@gmail.com>
2024-08-13 20:08:50 +0530

    Updates

---
+++ precache-manifest.4de15aba8a3a59e27cb50cf523719f31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7369a00cf4d73ecf49aa59573468033",
+    "url": "/TODO_LIST/index.html"
+  },
+  {
+    "revision": "5087c7188e72f32dd6f2",
+    "url": "/TODO_LIST/static/css/main.d30d7acf.chunk.css"
+  },
+  {
+    "revision": "62ef5994ecf0253aa998",
+    "url": "/TODO_LIST/static/js/2.5a022ba2.chunk.js"
+  },
+  {
+    "revision": "1ac23edd848720c87dd7eac90d67e14d",
+    "url": "/TODO_LIST/static/js/2.5a022ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de1d7ae4e2fcea45f7a8",
+    "url": "/TODO_LIST/static/js/3.7785d132.chunk.js"
+  },
+  {
+    "revision": "5087c7188e72f32dd6f2",
+    "url": "/TODO_LIST/static/js/main.9a8b3d79.chunk.js"
+  },
+  {
+    "revision": "a4572321524eb2cac83c",
+    "url": "/TODO_LIST/static/js/runtime-main.a5b52426.js"
+  }
+]);
